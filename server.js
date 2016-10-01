2408868459d604ecca9013384cbdd6ed51a20175
brianc <thehbc@uvic.ca>
2016-10-01 12:04:42 -0700

    Added style checking via eslint and formatted code to match style guidelines.
    The code style/standard being used here is from airbnb (https://github.com/airbnb/javascript). Running `npm run lint` will start the check and output any lint/style errors.

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-var express = require("express");
-var path = require("path");
-var bodyParser = require("body-parser");
-var mongodb = require("mongodb");
-var ObjectID = mongodb.ObjectID;
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const mongodb = require('mongodb');
+
+const ObjectID = mongodb.ObjectID;
 
 /**
  * Database Schema
@@ -45,15 +46,17 @@ var ObjectID = mongodb.ObjectID;
 
 // -------------- DATABASE SET UP ------------------------------
 
-var app = express();
-app.use(express.static(__dirname + "/public"));
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 
-// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
-var db;
+// Create a database variable outside of the database connection callback
+// to reuse the connection pool in your app.
+let db;
 
 // Connect to the database before starting the application server.
-mongodb.MongoClient.connect("mongodb://admin:seng480b@ds041556.mlab.com:41556/fridayideas", function(err, database) {
+const connString = 'mongodb://admin:seng480b@ds041556.mlab.com:41556/fridayideas';
+mongodb.MongoClient.connect(connString, (err, database) => {
   if (err) {
     console.log(err);
     process.exit(1);
@@ -61,22 +64,22 @@ mongodb.MongoClient.connect("mongodb://admin:seng480b@ds041556.mlab.com:41556/fr
 
   // Save database object from the callback for reuse.
   db = database;
-  console.log("Database connection ready");
+  console.log('Database connection ready');
 
   // Initialize the app.
-  var server = app.listen(process.env.PORT || 8080, function() {
-    var port = server.address().port;
-    console.log("App now running on port", port);
+  const server = app.listen(process.env.PORT || 8080, () => {
+    const port = server.address().port;
+    console.log('App now running on port', port);
   });
 });
 
 // -------------- PINS API BELOW -------------------------------
 
-var PINS_COLLECTION = "pins";
+const PINS_COLLECTION = 'pins';
 // Generic error handler used by all endpoints.
 function handleError(res, reason, message, code) {
-  console.log("ERROR: " + reason);
-  res.status(code || 500).json({"error": message});
+  console.log(`ERROR: ${reason}`);
+  res.status(code || 500).json({ error: message });
 }
 
 /**
@@ -92,29 +95,29 @@ function handleError(res, reason, message, code) {
  *   POST: creates a new pin
  */
 
-app.get("/api/pins", function(req, res) {
-  var searchArea = (req.query.searchArea || '').split(',');
-  var filters = searchArea.length == 4 ? {
+app.get('/api/pins', (req, res) => {
+  const searchArea = (req.query.searchArea || '').split(',');
+  const filters = searchArea.length === 4 ? {
     $and: [
-      {"coordinate.latitude": {$lte: searchArea[0]}},
-      {"coordinate.latitude": {$gte: searchArea[2]}},
-      {"coordinate.longitude": {$lte: searchArea[1]}},
-      {"coordinate.longitude": {$gte: searchArea[3]}}
-    ]
+      { 'coordinate.latitude': { $lte: searchArea[0] } },
+      { 'coordinate.latitude': { $gte: searchArea[2] } },
+      { 'coordinate.longitude': { $lte: searchArea[1] } },
+      { 'coordinate.longitude': { $gte: searchArea[3] } },
+    ],
   } : {};
 
   db.collection(PINS_COLLECTION).find(filters)
-    .toArray(function(err, docs) {
+    .toArray((err, docs) => {
       if (err) {
-        handleError(res, err.message, "Failed to get pins.");
+        handleError(res, err.message, 'Failed to get pins.');
       } else {
         res.status(200).json(docs);
       }
     });
 });
 
-app.post("/api/pins", function(req, res) {
-  var newPin = req.body;
+app.post('/api/pins', (req, res) => {
+  const newPin = req.body;
   newPin.createDate = new Date();
 
   newPin.likes = 0;
@@ -123,9 +126,9 @@ app.post("/api/pins", function(req, res) {
     newPin.duration = -1; // Default duration, means never ending event (such as a park)
   }
 
-  db.collection(PINS_COLLECTION).insertOne(newPin, function(err, doc) {
+  db.collection(PINS_COLLECTION).insertOne(newPin, (err, doc) => {
     if (err) {
-      handleError(res, err.message, "Failed to create new pin.");
+      handleError(res, err.message, 'Failed to create new pin.');
     } else {
       res.status(201).json(doc.ops[0]);
     }
@@ -138,33 +141,34 @@ app.post("/api/pins", function(req, res) {
  *    DELETE: deletes pin by id
  */
 
-app.get("/api/pins/:id", function(req, res) {
-  db.collection(PINS_COLLECTION).findOne({_id: new ObjectID(req.params.id)}, function(err, doc) {
+app.get('/api/pins/:id', (req, res) => {
+  db.collection(PINS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, (err, doc) => {
     if (err) {
-      handleError(res, err.message, "Failed to get pin");
+      handleError(res, err.message, 'Failed to get pin');
     } else {
       res.status(200).json(doc);
     }
   });
 });
 
-app.put("/api/pins/:id", function(req, res) {
-  var updateDoc = req.body;
+app.put('/api/pins/:id', (req, res) => {
+  const updateDoc = req.body;
   delete updateDoc._id;
 
-  db.collection(PINS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
-    if (err) {
-      handleError(res, err.message, "Failed to update pin");
-    } else {
-      res.status(204).end();
-    }
-  });
+  db.collection(PINS_COLLECTION)
+    .updateOne({ _id: new ObjectID(req.params.id) }, updateDoc, (err, doc) => {
+      if (err) {
+        handleError(res, err.message, 'Failed to update pin');
+      } else {
+        res.status(204).end();
+      }
+    });
 });
 
-app.delete("/api/pins/:id", function(req, res) {
-  db.collection(PINS_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
+app.delete('/api/pins/:id', (req, res) => {
+  db.collection(PINS_COLLECTION).deleteOne({ _id: new ObjectID(req.params.id) }, (err, result) => {
     if (err) {
-      handleError(res, err.message, "Failed to delete pin");
+      handleError(res, err.message, 'Failed to delete pin');
     } else {
       res.status(204).end();
     }
@@ -183,62 +187,65 @@ app.delete("/api/pins/:id", function(req, res) {
 // POST "/pins/like/:id/
 // Adds a like to the Pin ID
 
-app.post("/api/pins/:id/likes", function(req, res) {
-  var updateDoc = req.body;
+app.post('/api/pins/:id/likes', (req, res) => {
+  const updateDoc = req.body;
   delete updateDoc._id;
 
-  db.collection(PINS_COLLECTION).findOneAndUpdate({_id: new ObjectID(req.params.id)}, {$inc: {"likes": 1}},
-    function(err, doc) {
-      if (err) {
-        handleError(res, err.message, "Failed to update pin");
-      } else {
-        res.status(204).end();
-      }
-    });
+  db.collection(PINS_COLLECTION)
+    .findOneAndUpdate({ _id: new ObjectID(req.params.id) }, { $inc: { likes: 1 } },
+      (err, doc) => {
+        if (err) {
+          handleError(res, err.message, 'Failed to update pin');
+        } else {
+          res.status(204).end();
+        }
+      });
 });
 
 // POST "/pins/dislikes/:id/
 // Takes a like from the Pin ID
 
-app.post("/api/pins/:id/dislikes", function(req, res) {
-  var updateDoc = req.body;
+app.post('/api/pins/:id/dislikes', (req, res) => {
+  const updateDoc = req.body;
   delete updateDoc._id;
 
-  db.collection(PINS_COLLECTION).findOneAndUpdate({_id: new ObjectID(req.params.id)}, {$inc: {"likes": -1}},
-    function(err, doc) {
-      if (err) {
-        handleError(res, err.message, "Failed to update pin");
-      } else {
-        res.status(204).end();
-      }
-    });
+  db.collection(PINS_COLLECTION)
+    .findOneAndUpdate({ _id: new ObjectID(req.params.id) }, { $inc: { likes: -1 } },
+      (err, doc) => {
+        if (err) {
+          handleError(res, err.message, 'Failed to update pin');
+        } else {
+          res.status(204).end();
+        }
+      });
 });
 
 // POST Review with Pin ID
-app.post("/api/pins/:id/review", function(req, res) {
-  var updateDoc = req.body;
+app.post('/api/pins/:id/review', (req, res) => {
+  const updateDoc = req.body;
   delete updateDoc._id;
   updateDoc.createDate = new Date();
 
-  db.collection(PINS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, {$push: {reviews: updateDoc}},
-    function(err, doc) {
-      if (err) {
-        handleError(res, err.message, "Failed to add review to pin");
-      } else {
-        res.status(204).end();
-      }
-    });
+  db.collection(PINS_COLLECTION)
+    .updateOne({ _id: new ObjectID(req.params.id) }, { $push: { reviews: updateDoc } },
+      (err, doc) => {
+        if (err) {
+          handleError(res, err.message, 'Failed to add review to pin');
+        } else {
+          res.status(204).end();
+        }
+      });
 });
 
-// DELETE Review with Pin ID & Account IDs  
-app.post("/api/pins/:pinid/:accountid/review", function(req, res) {
-  console.log("Trying to remove from pin " + req.params.pinid + " review from account " + req.params.accountid)
+// DELETE Review with Pin ID & Account IDs
+app.post('/api/pins/:pinid/:accountid/review', (req, res) => {
+  console.log(`Trying to remove from pin ${req.params.pinid} review from account ${req.params.accountid}`);
 
-  db.collection(PINS_COLLECTION).update({_id: new ObjectID(req.params.pinid)},
-    {$pull: {reviews: {linkedAccount: parseInt(req.params.accountid)}}},
-    function(err, doc) {
+  db.collection(PINS_COLLECTION).update({ _id: new ObjectID(req.params.pinid) },
+    { $pull: { reviews: { linkedAccount: parseInt(req.params.accountid, 10) } } },
+    (err, doc) => {
       if (err) {
-        handleError(res, err.message, "Failed to remove review from pin");
+        handleError(res, err.message, 'Failed to remove review from pin');
       } else {
         res.status(204).end();
       }
@@ -246,13 +253,13 @@ app.post("/api/pins/:pinid/:accountid/review", function(req, res) {
 });
 
 // -------------- ACCOUNT API BELOW -------------------------
-var ACCOUNTS_COLLECTION = "accounts";
+const ACCOUNTS_COLLECTION = 'accounts';
 // GET Account
-app.get("/api/accounts/:id", function(req, res) {
+app.get('/api/accounts/:id', (req, res) => {
   db.collection(ACCOUNTS_COLLECTION)
-    .findOne({_id: new ObjectID(req.params.id)}, function(err, result) {
+    .findOne({ _id: new ObjectID(req.params.id) }, (err, result) => {
       if (err) {
-        handleError(res, err.message, "Failed to get account.");
+        handleError(res, err.message, 'Failed to get account.');
       } else {
         res.status(200).json(result);
       }
@@ -260,16 +267,16 @@ app.get("/api/accounts/:id", function(req, res) {
 });
 
 // POST Account
-app.post("/api/accounts", function(req, res) {
-  var newAccount = req.body;
+app.post('/api/accounts', (req, res) => {
+  const newAccount = req.body;
   newAccount.createDate = new Date();
 
   newAccount.numSeeds = 0;
   newAccount.numPins = 0;
 
-  db.collection(ACCOUNTS_COLLECTION).insertOne(newAccount, function(err, doc) {
+  db.collection(ACCOUNTS_COLLECTION).insertOne(newAccount, (err, doc) => {
     if (err) {
-      handleError(res, err.message, "Failed to create new account.");
+      handleError(res, err.message, 'Failed to create new account.');
     } else {
       res.status(201).json(doc.ops[0]);
     }
@@ -277,18 +284,17 @@ app.post("/api/accounts", function(req, res) {
 });
 
 // PUT Account Password
-app.put("/api/accounts/:id/:password", function(req, res) {
-
+app.put('/api/accounts/:id/:password', (req, res) => {
   if (req.body.password) {
-    handleError(res, "Invalid user input", "Must provide new password in request body.", 400);
+    handleError(res, 'Invalid user input', 'Must provide new password in request body.', 400);
   }
 
-  var newPassword = req.body.password;
-  db.collection(ACCOUNTS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)},
-    {$set: {"password": newPassword}},
-    function(err, doc) {
+  const newPassword = req.body.password;
+  db.collection(ACCOUNTS_COLLECTION).updateOne({ _id: new ObjectID(req.params.id) },
+    { $set: { password: newPassword } },
+    (err, doc) => {
       if (err) {
-        handleError(res, err.message, "Failed to update seed amount for account");
+        handleError(res, err.message, 'Failed to update seed amount for account');
       } else {
         res.status(204).end();
       }
@@ -297,12 +303,12 @@ app.put("/api/accounts/:id/:password", function(req, res) {
 
 // PUT Account Seed Amount
 // TODO this ain't terribly RESTful.
-app.put("/api/accounts/seeds/:id/:amount", function(req, res) {
-  db.collection(ACCOUNTS_COLLECTION).findOneAndUpdate({_id: new ObjectID(req.params.id)},
-    {$inc: {"numSeeds": req.params.amount}},
-    function(err, doc) {
+app.put('/api/accounts/seeds/:id/:amount', (req, res) => {
+  db.collection(ACCOUNTS_COLLECTION).findOneAndUpdate({ _id: new ObjectID(req.params.id) },
+    { $inc: { numSeeds: req.params.amount } },
+    (err, doc) => {
       if (err) {
-        handleError(res, err.message, "Failed to update seed amount for account");
+        handleError(res, err.message, 'Failed to update seed amount for account');
       } else {
         res.status(204).end();
       }
