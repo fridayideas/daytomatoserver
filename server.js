b4c39f7c1bae98897fa8a36ab47b78aadafb4623
brianc <thehbc@uvic.ca>
2016-10-01 10:55:42 -0700

    Formatting and docs changes.
    The database schema should be properly represented in the doc comments now (my bad!). Hopefully we'll be able to converge on some form of consistent code style soon.

--- server.js
+++ server.js
@@ -4,53 +4,44 @@ var bodyParser = require("body-parser");
 var mongodb = require("mongodb");
 var ObjectID = mongodb.ObjectID;
 
-/*
-Database Schema
-// PINS
-{
-  "_id": <ObjectId>
-  "rating": <string>,
-  "pinType": <int>,
-  "name": <string>,
-  "description": <string>,
-  "likes" : <int>,
-  "duration": <long>,
-  "coordinate": {
-    "latitude": <double>,
-    "longitude": <double>
-  },
-  "linkedAccount": <ObjectId>, 
-  "reviews": [{"linkedAccount":<ObjectId>,"text":<string>,"createDate":<Date>}...]
-}
-
-//REVIEWS
-{
-  "_id": <ObjectId>
-  "linkedPin": <ObjectId>,
-  "linkedAccount": <ObjectId>, 
-  "text": <string>
-  "linkedAccount": <ObjectId>,
-  "reviews": [ {
-        "linkedAccount": <ObjectId>,
-        "text": <string>,
-        "date": <DateTime>
-        },
-        {
-        "linkedAccount": <ObjectId>,
-        "text": <string>,
-        "date": <DateTime>
-        } ...
-      ]
-}
-
-//ACCOUNTS
-{
-  "_id": <ObjectId>,
-  "username": <string>,
-  "seeds": <double>,
-  "pins": <int>
-}
-*/
+/**
+ * Database Schema
+ * // PINS
+ * {
+ *   "_id": <ObjectId>
+ *   "rating": <string>,
+ *   "pinType": <int>,
+ *   "name": <string>,
+ *   "description": <string>,
+ *   "likes" : <int>,
+ *   "duration": <long>,
+ *   "coordinate": {
+ *     "latitude": <double>,
+ *     "longitude": <double>
+ *   },
+ *   "linkedAccount": <ObjectId>,
+ *   "reviews": [
+ *     {
+ *       "linkedAccount": <ObjectId>,
+ *       "text": <string>,
+ *       "date": <DateTime>
+ *     },
+ *     {
+ *       "linkedAccount": <ObjectId>,
+ *       "text": <string>,
+ *       "date": <DateTime>
+ *     } ...
+ *   ]
+ * }
+ *
+ * //ACCOUNTS
+ * {
+ *   "_id": <ObjectId>,
+ *   "username": <string>,
+ *   "seeds": <double>,
+ *   "pins": <int>
+ * }
+ */
 
 // -------------- DATABASE SET UP ------------------------------
 
@@ -62,7 +53,7 @@ app.use(bodyParser.json());
 var db;
 
 // Connect to the database before starting the application server.
-mongodb.MongoClient.connect("mongodb://admin:seng480b@ds041556.mlab.com:41556/fridayideas", function (err, database) {
+mongodb.MongoClient.connect("mongodb://admin:seng480b@ds041556.mlab.com:41556/fridayideas", function(err, database) {
   if (err) {
     console.log(err);
     process.exit(1);
@@ -73,7 +64,7 @@ mongodb.MongoClient.connect("mongodb://admin:seng480b@ds041556.mlab.com:41556/fr
   console.log("Database connection ready");
 
   // Initialize the app.
-  var server = app.listen(process.env.PORT || 8080, function () {
+  var server = app.listen(process.env.PORT || 8080, function() {
     var port = server.address().port;
     console.log("App now running on port", port);
   });
@@ -88,26 +79,27 @@ function handleError(res, reason, message, code) {
   res.status(code || 500).json({"error": message});
 }
 
-/*  "/pins"
- *    GET: finds all pins
- *      query params:
- *        searchArea (optional): a set of coordinates to search in. The format is
- *          `{topLeftLat},{topLeftLong},{bottomRightLat},{bottomRightLong}`, where:
- *          topLeftLat = latitude of the top left of the bounding box
- *          topLeftLong = longitude of the top left of the bounding box
- *          bottomRightLat = latitude of the bottom right of the bounding box
- *          bottomRightLong = longitude of the bottom right of the bounding box
- *    POST: creates a new pin
+/**
+ * "/pins"
+ *   GET: finds all pins
+ *     query params:
+ *       searchArea (optional): a set of coordinates to search in. The format is
+ *         `{topLeftLat},{topLeftLong},{bottomRightLat},{bottomRightLong}`, where:
+ *         topLeftLat = latitude of the top left of the bounding box
+ *         topLeftLong = longitude of the top left of the bounding box
+ *         bottomRightLat = latitude of the bottom right of the bounding box
+ *         bottomRightLong = longitude of the bottom right of the bounding box
+ *   POST: creates a new pin
  */
 
 app.get("/api/pins", function(req, res) {
   var searchArea = (req.query.searchArea || '').split(',');
   var filters = searchArea.length == 4 ? {
     $and: [
-      { "coordinate.latitude"  : { $lte: searchArea[0] } },
-      { "coordinate.latitude"  : { $gte: searchArea[2] } },
-      { "coordinate.longitude" : { $lte: searchArea[1] } },
-      { "coordinate.longitude" : { $gte: searchArea[3] } }
+      {"coordinate.latitude": {$lte: searchArea[0]}},
+      {"coordinate.latitude": {$gte: searchArea[2]}},
+      {"coordinate.longitude": {$lte: searchArea[1]}},
+      {"coordinate.longitude": {$gte: searchArea[3]}}
     ]
   } : {};
 
@@ -127,7 +119,7 @@ app.post("/api/pins", function(req, res) {
 
   newPin.likes = 0;
   newPin.reviews = [];
-  if (!req.body.duration){
+  if (!req.body.duration) {
     newPin.duration = -1; // Default duration, means never ending event (such as a park)
   }
 
@@ -147,7 +139,7 @@ app.post("/api/pins", function(req, res) {
  */
 
 app.get("/api/pins/:id", function(req, res) {
-  db.collection(PINS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+  db.collection(PINS_COLLECTION).findOne({_id: new ObjectID(req.params.id)}, function(err, doc) {
     if (err) {
       handleError(res, err.message, "Failed to get pin");
     } else {
@@ -179,29 +171,14 @@ app.delete("/api/pins/:id", function(req, res) {
   });
 });
 
-/*  "/pins/:topLeftLat/:topLeftLong/:bottomRightLat/:bottomRightLong"
- *    GET: Find pins inside specified coordinades.
-        topLeftLat : latitude of the top left of the bounding box
-        topLeftLong : longitude of the top left of the bounding box
-        bottomRightLat : latitude of the bottom right of the bounding box
-        bottomRightLong : longitude of the bottom right of the bounding box
-
-app.get("/api/pins/:topLeftLat/:topLeftLong/:bottomRightLat/:bottomRightLong", function(req, res) {
-  db.collection(PINS_COLLECTION)
-      .find({
-          $and: [ { "coordinate.latitude"  : { $gte: req.params.bottomRightLat  } } ,
-                  { "coordinate.latitude"  : { $lte: req.params.topLeftLat      } } ,
-                  { "coordinate.longitude" : { $lte: req.params.topLeftLong     } } ,
-                  { "coordinate.longitude" : { $gte: req.params.bottomRightLong } } ] } )
-      .toArray(function(err, docs) {
-          if (err) {
-            handleError(res, err.message, "Failed to get contacts.");
-          } else {
-            res.status(200).json(docs);
-          }
-      });
-});
-*/
+/**
+ * "/pins/:topLeftLat/:topLeftLong/:bottomRightLat/:bottomRightLong"
+ *   GET: Find pins inside specified coordinates.
+ *     topLeftLat : latitude of the top left of the bounding box
+ *     topLeftLong : longitude of the top left of the bounding box
+ *     bottomRightLat : latitude of the bottom right of the bounding box
+ *     bottomRightLong : longitude of the bottom right of the bounding box
+ */
 
 // POST "/pins/like/:id/
 // Adds a like to the Pin ID
@@ -210,14 +187,14 @@ app.post("/api/pins/:id/likes", function(req, res) {
   var updateDoc = req.body;
   delete updateDoc._id;
 
-  db.collection(PINS_COLLECTION).findOneAndUpdate( {_id: new ObjectID(req.params.id)} , { $inc: { "likes" : 1 } } ,
+  db.collection(PINS_COLLECTION).findOneAndUpdate({_id: new ObjectID(req.params.id)}, {$inc: {"likes": 1}},
     function(err, doc) {
       if (err) {
         handleError(res, err.message, "Failed to update pin");
       } else {
         res.status(204).end();
       }
-  });
+    });
 });
 
 // POST "/pins/dislikes/:id/
@@ -227,14 +204,14 @@ app.post("/api/pins/:id/dislikes", function(req, res) {
   var updateDoc = req.body;
   delete updateDoc._id;
 
-  db.collection(PINS_COLLECTION).findOneAndUpdate( {_id: new ObjectID(req.params.id)} , { $inc: { "likes" : -1 } } ,
+  db.collection(PINS_COLLECTION).findOneAndUpdate({_id: new ObjectID(req.params.id)}, {$inc: {"likes": -1}},
     function(err, doc) {
       if (err) {
         handleError(res, err.message, "Failed to update pin");
       } else {
         res.status(204).end();
       }
-  });
+    });
 });
 
 // POST Review with Pin ID
@@ -243,42 +220,43 @@ app.post("/api/pins/:id/review", function(req, res) {
   delete updateDoc._id;
   updateDoc.createDate = new Date();
 
-  db.collection(PINS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, { $push: {reviews: updateDoc } },
+  db.collection(PINS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, {$push: {reviews: updateDoc}},
     function(err, doc) {
-    if (err) {
-      handleError(res, err.message, "Failed to add review to pin");
-    } else {
-      res.status(204).end();
-    }
-  });
+      if (err) {
+        handleError(res, err.message, "Failed to add review to pin");
+      } else {
+        res.status(204).end();
+      }
+    });
 });
 
 // DELETE Review with Pin ID & Account IDs  
 app.post("/api/pins/:pinid/:accountid/review", function(req, res) {
   console.log("Trying to remove from pin " + req.params.pinid + " review from account " + req.params.accountid)
 
-  db.collection(PINS_COLLECTION).update({_id: new ObjectID(req.params.pinid)}, 
-    { $pull: { reviews: { linkedAccount : parseInt(req.params.accountid) } } },
+  db.collection(PINS_COLLECTION).update({_id: new ObjectID(req.params.pinid)},
+    {$pull: {reviews: {linkedAccount: parseInt(req.params.accountid)}}},
     function(err, doc) {
-    if (err) {
-      handleError(res, err.message, "Failed to remove review from pin");
-    } else {
-      res.status(204).end();
-    }
-  });
+      if (err) {
+        handleError(res, err.message, "Failed to remove review from pin");
+      } else {
+        res.status(204).end();
+      }
+    });
 });
 
 // -------------- ACCOUNT API BELOW -------------------------
 var ACCOUNTS_COLLECTION = "accounts";
 // GET Account
 app.get("/api/accounts/:id", function(req, res) {
-  db.collection(ACCOUNTS_COLLECTION).findOne({_id:new ObjectID(req.params.id)}, function(err, result) {
-    if (err) {
-      handleError(res, err.message, "Failed to get account.");
-    } else {
-      res.status(200).json(result);
-    }
-  });
+  db.collection(ACCOUNTS_COLLECTION)
+    .findOne({_id: new ObjectID(req.params.id)}, function(err, result) {
+      if (err) {
+        handleError(res, err.message, "Failed to get account.");
+      } else {
+        res.status(200).json(result);
+      }
+    });
 });
 
 // POST Account
@@ -302,32 +280,32 @@ app.post("/api/accounts", function(req, res) {
 app.put("/api/accounts/:id/:password", function(req, res) {
 
   if (req.body.password) {
-      handleError(res, "Invalid user input", "Must provide new password in request body.", 400);
+    handleError(res, "Invalid user input", "Must provide new password in request body.", 400);
   }
 
   var newPassword = req.body.password;
-  db.collection(ACCOUNTS_COLLECTION).updateOne( {_id: new ObjectID(req.params.id)} ,
-    { $set: { "password" : newPassword } } ,
+  db.collection(ACCOUNTS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)},
+    {$set: {"password": newPassword}},
     function(err, doc) {
       if (err) {
         handleError(res, err.message, "Failed to update seed amount for account");
       } else {
         res.status(204).end();
       }
-  });
+    });
 });
 
 // PUT Account Seed Amount
 // TODO this ain't terribly RESTful.
 app.put("/api/accounts/seeds/:id/:amount", function(req, res) {
-  db.collection(ACCOUNTS_COLLECTION).findOneAndUpdate( {_id: new ObjectID(req.params.id)} ,
-    { $inc: { "numSeeds" : req.params.amount } } ,
+  db.collection(ACCOUNTS_COLLECTION).findOneAndUpdate({_id: new ObjectID(req.params.id)},
+    {$inc: {"numSeeds": req.params.amount}},
     function(err, doc) {
       if (err) {
         handleError(res, err.message, "Failed to update seed amount for account");
       } else {
         res.status(204).end();
       }
-  });
+    });
 });
 
