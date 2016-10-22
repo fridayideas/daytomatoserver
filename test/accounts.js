3cce79cae6f76da982564d3e2f4eaedef3c3c2ca
Brian Chen <brianchen.czq@gmail.com>
2016-10-22 12:42:44 -0700

    Add  authenticated endpoint for getting current user account.
    `GET /accounts/currentuser` will create a new database user account on first sign in (i.e. we do not have a daytomato account for a user). This ensures grabbing user profile data for the home/profile view only requires one api call.

--- test/accounts.js
+++ test/accounts.js
@@ -8,4 +8,57 @@ const expect = chai.expect;
 require('dotenv').config();
 const server = require('../index');
 
-describe('Accounts', () => {});
+describe('Accounts', () => {
+  let app;
+  let db;
+  let pins;
+  let accounts;
+  before(() => server.connect(5001).then((res) => {
+    app = res.app;
+    db = res.db;
+    pins = db.collection('pins');
+    accounts = db.collection('accounts');
+    return db;
+  }));
+
+  // reset db
+  afterEach(() => pins.deleteMany());
+  afterEach(() => accounts.deleteMany());
+
+  describe('GET Accounts', () => {
+    it('should create a new user account if none exists', () =>
+      chai.request(app)
+        .get('/api/accounts/currentuser')
+        .set('Authorization', `Bearer ${process.env.TEST_JWT}`)
+        .then((res) => {
+          expect(res).to.have.status(200);
+          const account = res.body;
+          expect(account.createDate).to.be.not.empty;
+          expect(account.numSeeds).to.equal(0);
+          expect(account.numPins).to.equal(0);
+        })
+        .catch((err) => {
+          throw err;
+        }));
+    it('should find an existing user account', () =>
+      accounts.insertOne({
+        auth0Id: process.env.TEST_USER_SUB,
+        createDate: new Date(),
+        numSeeds: 10,
+        numPins: 5,
+      }).then(res => res.ops[0]).then(acct =>
+        chai.request(app)
+          .get('/api/accounts/currentuser')
+          .set('Authorization', `Bearer ${process.env.TEST_JWT}`)
+          .then((res) => {
+            expect(res).to.have.status(200);
+            const account = res.body;
+            expect(new Date(account.createDate)).to.eql(acct.createDate);
+            expect(account.numSeeds).to.equal(acct.numSeeds);
+            expect(account.numPins).to.equal(acct.numPins);
+          })
+      ).catch((err) => {
+        throw err;
+      }));
+  });
+});
